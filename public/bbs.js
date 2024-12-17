6b9f9fa463506c4ad389238924ef38c3e701e157
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2024-12-17 09:20:38 +0900

    bbs追加

---
+++ public/bbs.js
@@ -0,0 +1,90 @@
+"use strict";
+
+let number=0;
+const bbs = document.querySelector('#bbs');
+document.querySelector('#post').addEventListener('click', () => {
+    const name = document.querySelector('#name').value;
+    const message = document.querySelector('#message').value;
+
+    const params = {  // URL Encode
+        method: "POST",
+        body:  'name='+name+'&message='+message,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+    console.log( params );
+    const url = "/post";
+    fetch( url, params )
+    .then( (response) => {
+        if( !response.ok ) {
+            throw new Error('Error');
+        }
+        return response.json();
+    })
+    .then( (response) => {
+        console.log( response );
+        document.querySelector('#message').value = "";
+    });
+});
+
+document.querySelector('#check').addEventListener('click', () => {
+    const params = {  // URL Encode
+        method: "POST",
+        body:  '',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+    const url = "/check";
+    fetch( url, params )
+    .then( (response) => {
+        if( !response.ok ) {
+            throw new Error('Error');
+        }
+        return response.json();
+    })
+    .then( (response) => {
+        let value = response.number;
+        console.log( value );
+
+        console.log( number );
+        if( number != value ) {
+            const params = {
+                method: "POST",
+                body: 'start='+number,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'               
+                }
+            }
+            const url = "/read";
+            fetch( url, params )
+            //エラー処理
+            .then( (response) => {
+                if( !response.ok ) {
+                    throw new Error('Error');
+                }
+                return response.json();
+            })
+            //投稿内容を表示
+            .then( (response) => {
+                number += response.messages.length;
+                for( let mes of response.messages ) {
+                    console.log( mes );  // 表示する投稿
+                    let cover = document.createElement('div');
+                    cover.className = 'cover';
+                    let name_area = document.createElement('span');
+                    name_area.className = 'name';
+                    name_area.innerText = mes.name;
+                    let mes_area = document.createElement('span');
+                    mes_area.className = 'mes';
+                    mes_area.innerText = mes.message;
+                    cover.appendChild( name_area );
+                    cover.appendChild( mes_area );
+
+                    bbs.appendChild( cover );
+                }
+            })
+        }
+    });
+});
