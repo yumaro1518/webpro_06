dcf7afeacfb17f73ef7a699d84ef72a730aba732
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2024-12-24 02:24:35 +0900

    各テンプレートファイルにstyle要素を追加

--- app5.js
+++ app5.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static('public'));
 
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
